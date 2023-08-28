3494c1ebcecb3e64c066e4bffd7435968216dd9f
Fernando <fernandosmartphone96@gmail.com>
2023-08-28 08:20:39 -0300

    Cria Posicao

---
+++ src/aplicacao/boardgame/Posicao.java
@@ -0,0 +1,27 @@
+package aplicacao.boardgame;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@Setter
+public class Posicao {
+    
+    private Integer linha;
+    private Integer coluna;
+
+    @Override
+    public String toString() {
+        return "%d, %d".formatted(linha, coluna);
+    }
+
+    /*
+     * String mensagem = MessageFormat.format("{0} tem {1} {1,choice,1#ano|1<anos}", nome, idade);
+     * https://pt.stackoverflow.com/questions/512957/h%C3%A1-como-interpolar-strings-em-java
+     */
+}
