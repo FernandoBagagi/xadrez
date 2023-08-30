86b4903526cc8cee191ab45b2fab639a4cf8379f
Fernando <fernandosmartphone96@gmail.com>
2023-08-30 08:53:32 -0300

    cria PartidaXadrez

---
+++ src/aplicacao/chess/PartidaXadrez.java
@@ -0,0 +1,26 @@
+package aplicacao.chess;
+
+import aplicacao.boardgame.Tabuleiro;
+
+public class PartidaXadrez {
+    
+    Tabuleiro tabuleiro;
+
+    public PartidaXadrez(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+    }
+
+    public PecaXadrez[][] getPecasXadrez() {
+        final int linhas = this.tabuleiro.getLinhas();
+        final int colunas = this.tabuleiro.getColunas();
+        PecaXadrez[][] tabuleiroXadrez = new PecaXadrez[linhas][colunas];
+        for(int i = 0; i < linhas; i++) {
+            for(int j = 0; j < colunas; j++) {
+                tabuleiroXadrez[i][j] = (PecaXadrez) this.tabuleiro.getPeca(i, j);
+            }
+        }
+        
+        return tabuleiroXadrez;
+    }
+
+}
