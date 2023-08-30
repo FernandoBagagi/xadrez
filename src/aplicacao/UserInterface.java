ec1195dc1676705f409936cef8cdd14bd44d5321
Fernando <fernandosmartphone96@gmail.com>
2023-08-30 09:04:20 -0300

    cria UserInterface

---
+++ src/aplicacao/UserInterface.java
@@ -0,0 +1,26 @@
+package aplicacao;
+
+import aplicacao.chess.PecaXadrez;
+
+public class UserInterface {
+    
+    public static void imprimirTabuleiro(PecaXadrez[][] pecas) {
+        for(int i = 0; i < pecas.length; i++) {
+            System.out.print((8 - i) + " ");
+            for(int j = 0; j < pecas.length; j++) {
+                UserInterface.imprimirPeca(pecas[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.print("  a b c d e f g h");
+    }
+
+    private static void imprimirPeca(PecaXadrez peca) {
+        if(peca == null) {
+            System.out.print(" ");
+        } else {
+            System.out.print(peca);
+        }
+        System.out.print(" ");
+    }
+}
