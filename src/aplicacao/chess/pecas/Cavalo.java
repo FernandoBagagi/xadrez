2275f17527efd401e4ba9b7b091430d69eb01d1b
Fernando <fernandosmartphone96@gmail.com>
2023-08-31 08:03:52 -0300

    Cria Cavalo

---
+++ src/aplicacao/chess/pecas/Cavalo.java
@@ -0,0 +1,18 @@
+package aplicacao.chess.pecas;
+
+import aplicacao.boardgame.Tabuleiro;
+import aplicacao.chess.Cor;
+import aplicacao.chess.PecaXadrez;
+
+public class Cavalo extends PecaXadrez{
+
+    public Cavalo(Tabuleiro tabuleiro, Cor cor) {
+        super(tabuleiro, cor);
+    }
+
+    @Override
+    public String toString() {
+        return "C";
+    }
+    
+}
