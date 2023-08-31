f7f92f6d500144930cdd39cd00919c01518298df
Fernando <fernandosmartphone96@gmail.com>
2023-08-31 08:03:39 -0300

    Cria Bispo

---
+++ src/aplicacao/chess/pecas/Bispo.java
@@ -0,0 +1,18 @@
+package aplicacao.chess.pecas;
+
+import aplicacao.boardgame.Tabuleiro;
+import aplicacao.chess.Cor;
+import aplicacao.chess.PecaXadrez;
+
+public class Bispo extends PecaXadrez{
+
+    public Bispo(Tabuleiro tabuleiro, Cor cor) {
+        super(tabuleiro, cor);
+    }
+
+    @Override
+    public String toString() {
+        return "B";
+    }
+
+}
