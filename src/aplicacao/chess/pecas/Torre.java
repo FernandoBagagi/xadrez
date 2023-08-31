69b7677abe683657965468adde0a6375d3c7650a
Fernando <fernandosmartphone96@gmail.com>
2023-08-31 08:04:23 -0300

    Cria Torre

---
+++ src/aplicacao/chess/pecas/Torre.java
@@ -0,0 +1,18 @@
+package aplicacao.chess.pecas;
+
+import aplicacao.boardgame.Tabuleiro;
+import aplicacao.chess.Cor;
+import aplicacao.chess.PecaXadrez;
+
+public class Torre extends PecaXadrez{
+
+    public Torre(Tabuleiro tabuleiro, Cor cor) {
+        super(tabuleiro, cor);
+    }
+
+    @Override
+    public String toString() {
+        return "T";
+    }
+    
+}
