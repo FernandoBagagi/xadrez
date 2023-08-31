cae6ac770757d3504322b8f1257efd03fb83ed4e
Fernando <fernandosmartphone96@gmail.com>
2023-08-31 08:03:06 -0300

    cria posicionarPecasInicio

--- src/aplicacao/chess/PartidaXadrez.java
+++ src/aplicacao/chess/PartidaXadrez.java
@@ -1,6 +1,8 @@
 package aplicacao.chess;
 
+import aplicacao.boardgame.Posicao;
 import aplicacao.boardgame.Tabuleiro;
+import aplicacao.chess.pecas.*;
 
 public class PartidaXadrez {
     
@@ -8,6 +10,7 @@ public class PartidaXadrez {
 
     public PartidaXadrez() {
         this.tabuleiro = new Tabuleiro(8, 8);
+        this.posicionarPecasInicio();
     }
 
     public PecaXadrez[][] getPecasXadrez() {
@@ -23,4 +26,16 @@ public class PartidaXadrez {
         return tabuleiroXadrez;
     }
 
+    private void posicionarPecasInicio() {
+        this.tabuleiro.posicionarPeca(new Torre(this.tabuleiro, Cor.PRETO), new Posicao(0, 0));
+        this.tabuleiro.posicionarPeca(new Cavalo(this.tabuleiro, Cor.PRETO), new Posicao(0, 1));
+        this.tabuleiro.posicionarPeca(new Bispo(this.tabuleiro, Cor.PRETO), new Posicao(0, 2));
+        this.tabuleiro.posicionarPeca(new Rainha(this.tabuleiro, Cor.PRETO), new Posicao(0, 3));
+        this.tabuleiro.posicionarPeca(new Rei(this.tabuleiro, Cor.PRETO), new Posicao(0, 4));
+        this.tabuleiro.posicionarPeca(new Bispo(this.tabuleiro, Cor.PRETO), new Posicao(0, 5));
+        this.tabuleiro.posicionarPeca(new Cavalo(this.tabuleiro, Cor.PRETO), new Posicao(0, 6));
+        this.tabuleiro.posicionarPeca(new Torre(this.tabuleiro, Cor.PRETO), new Posicao(0, 7));
+
+        
+    }
 }
