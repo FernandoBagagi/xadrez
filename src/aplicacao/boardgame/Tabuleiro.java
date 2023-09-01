98849799158aee043d3fe87e7346296d4dab9286
Fernando <fernandosmartphone96@gmail.com>
2023-09-01 09:38:49 -0300

    faz programação defensiva

--- src/aplicacao/boardgame/Tabuleiro.java
+++ src/aplicacao/boardgame/Tabuleiro.java
@@ -5,6 +5,12 @@ public class Tabuleiro {
     private Peca[][] pecas;
 
     public Tabuleiro(int linhas, int colunas) {
+        if(linhas < 1) {
+            throw new TabuleiroExcecao("O número de linhas deve ser maior que 0");            
+        }
+        if(colunas < 1) {
+            throw new TabuleiroExcecao("O número de colunas deve ser maior que 0");            
+        }
         this.pecas = new Peca[linhas][colunas];
     }
 
@@ -21,15 +27,20 @@ public class Tabuleiro {
     }
 
     public Peca getPeca(int linha, int coluna) {
-        return this.pecas[linha][coluna];
+        if(this.posicaoExiste(linha, coluna)) {
+            return this.pecas[linha][coluna];
+        }
+        return null; //Deve ter exceção e não retorna null
     }
 
     public Peca getPeca(Posicao posicao) {
         return this.getPeca(posicao.getLinha(), posicao.getColuna());
     }
     
-    
     public void posicionarPeca(Peca peca, Posicao posicao) {
+        if(this.existeUmaPecaNaPosicao(posicao)) {
+            throw new TabuleiroExcecao("Já existe outra peça na posição " + posicao);
+        }
         this.pecas[posicao.getLinha()][posicao.getColuna()] = peca;
         peca.posicao = posicao; //Consegue acessar pois é protected
     }
@@ -41,8 +52,14 @@ public class Tabuleiro {
 
     private boolean posicaoExiste(int linha, int coluna) {
         boolean linhaExiste = linha >= 0 && linha < this.getLinhas();
+        if(!linhaExiste) {
+            throw new TabuleiroExcecao("Posição inválida! A linha " + linha + " não existe no tabuleiro!");
+        }
         boolean colunaExiste = coluna >= 0 && coluna < this.getColunas();
-        return linhaExiste && colunaExiste;
+        if(!colunaExiste) {
+            throw new TabuleiroExcecao("Posição inválida! A coluna " + coluna + " não existe no tabuleiro!");
+        }
+        return true;
     }
 
     public boolean posicaoExiste(Posicao posicao) {
