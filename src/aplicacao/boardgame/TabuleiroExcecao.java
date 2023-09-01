e67d19da5a62f2d542bb2c329758defba62f0e4a
Fernando <fernandosmartphone96@gmail.com>
2023-09-01 09:05:57 -0300

    implementa TabuleiroExcecao

--- src/aplicacao/boardgame/TabuleiroExcecao.java
+++ src/aplicacao/boardgame/TabuleiroExcecao.java
@@ -2,4 +2,10 @@ package aplicacao.boardgame;
 
 public class TabuleiroExcecao extends RuntimeException{
     
+    private static final long serialVersionUID = 1L;
+
+    public TabuleiroExcecao(String mensagemErro) {
+        super(mensagemErro);
+    }
+
 }
