7a417d39d419abad2621a6a5932aa6ed6242dc2c
Fernando Nomus <fernandosmartphone96@gmail.com>
2023-09-06 14:58:51 -0300

    muda herança

--- src/aplicacao/chess/XadrezExcecao.java
+++ src/aplicacao/chess/XadrezExcecao.java
@@ -1,6 +1,8 @@
 package aplicacao.chess;
 
-public class XadrezExcecao extends RuntimeException {
+import aplicacao.boardgame.TabuleiroExcecao;
+
+public class XadrezExcecao extends TabuleiroExcecao {
 
     private static final long serialVersionUID = 1L;
 
