e44570d6aa8337fb834704d842ffb7b6751bab83
Fernando Nomus <fernandosmartphone96@gmail.com>
2023-09-11 10:54:54 -0300

    cria lógica da main

--- src/aplicacao/App.java
+++ src/aplicacao/App.java
@@ -1,11 +1,34 @@
 package aplicacao;
 
+import java.util.Scanner;
+
 import aplicacao.chess.PartidaXadrez;
+import aplicacao.chess.PecaXadrez;
+import aplicacao.chess.PosicaoXadrez;
 
 public class App {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        PartidaXadrez partidaXadrez = new PartidaXadrez();
         
-        //PartidaXadrez partidaXadrez = new PartidaXadrez();
+        while (true) {
+            
+            UserInterface.limparTela();
+
+            UserInterface.imprimirTabuleiro(partidaXadrez.getPecasXadrez());
+            
+            System.out.print("\nOrigem: ");
+            PosicaoXadrez origem = UserInterface.lerPosicaoXadrez(scanner);
+
+            System.out.print("\nDestino: ");
+            PosicaoXadrez destino = UserInterface.lerPosicaoXadrez(scanner);
+            System.out.println();
+            
+            PecaXadrez pecaCapturada = partidaXadrez.moverPecaXadrez(origem, destino);
+
+        }
+
         //UserInterface.imprimirTabuleiro(partidaXadrez.getPecasXadrez());
     }
     /*Não zerar */
