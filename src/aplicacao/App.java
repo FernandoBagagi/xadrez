786e6d1462803fff0ee6bc05e6ba305a966496c4
Fernando Nomus <fernandosmartphone96@gmail.com>
2023-09-11 10:20:06 -0300

    limpa main

--- src/aplicacao/App.java
+++ src/aplicacao/App.java
@@ -3,9 +3,10 @@ package aplicacao;
 import aplicacao.chess.PartidaXadrez;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        PartidaXadrez partidaXadrez = new PartidaXadrez();
-        UserInterface.imprimirTabuleiro(partidaXadrez.getPecasXadrez());
+    public static void main(String[] args) {
+        
+        //PartidaXadrez partidaXadrez = new PartidaXadrez();
+        //UserInterface.imprimirTabuleiro(partidaXadrez.getPecasXadrez());
     }
     /*Não zerar */
     /*Não zerar 2*/
