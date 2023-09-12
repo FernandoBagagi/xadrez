77483d194b84595fd0c35f3cc85d1092e96714ac
Fernando Nomus <fernandosmartphone96@gmail.com>
2023-09-12 15:47:05 -0300

    faz tratamento de exceção

--- src/aplicacao/App.java
+++ src/aplicacao/App.java
@@ -1,10 +1,12 @@
 package aplicacao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import aplicacao.chess.PartidaXadrez;
 import aplicacao.chess.PecaXadrez;
 import aplicacao.chess.PosicaoXadrez;
+import aplicacao.chess.XadrezExcecao;
 
 public class App {
     public static void main(String[] args) {
@@ -13,20 +15,23 @@ public class App {
         PartidaXadrez partidaXadrez = new PartidaXadrez();
         
         while (true) {
-            
-            UserInterface.limparTela();
-
-            UserInterface.imprimirTabuleiro(partidaXadrez.getPecasXadrez());
-            
-            System.out.print("\nOrigem: ");
-            PosicaoXadrez origem = UserInterface.lerPosicaoXadrez(scanner);
-
-            System.out.print("\nDestino: ");
-            PosicaoXadrez destino = UserInterface.lerPosicaoXadrez(scanner);
-            System.out.println();
-            
-            PecaXadrez pecaCapturada = partidaXadrez.moverPecaXadrez(origem, destino);
-
+            try {
+                UserInterface.limparTela();
+    
+                UserInterface.imprimirTabuleiro(partidaXadrez.getPecasXadrez());
+                
+                System.out.print("\nOrigem: ");
+                PosicaoXadrez origem = UserInterface.lerPosicaoXadrez(scanner);
+    
+                System.out.print("\nDestino: ");
+                PosicaoXadrez destino = UserInterface.lerPosicaoXadrez(scanner);
+                System.out.println();
+                
+                PecaXadrez pecaCapturada = partidaXadrez.moverPecaXadrez(origem, destino);    
+            } catch (XadrezExcecao | InputMismatchException e) {
+                System.err.println(e.getMessage() + " Pressione enter para continuar");
+                scanner.nextLine(); //Esperar usuário da enter
+            }
         }
 
         //UserInterface.imprimirTabuleiro(partidaXadrez.getPecasXadrez());
