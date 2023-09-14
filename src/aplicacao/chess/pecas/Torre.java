84bf986d3a1d7f5ca9e8f38ffc74131d8c136560
Fernando Nomus <fernandosmartphone96@gmail.com>
2023-09-14 09:43:01 -0300

    cria moverParaCima

--- src/aplicacao/chess/pecas/Torre.java
+++ src/aplicacao/chess/pecas/Torre.java
@@ -22,14 +22,33 @@ public class Torre extends PecaXadrez {
         final int colunas = this.getTabuleiro().getColunas();
         boolean[][] matrizPossiveisMovimentos = new boolean[linhas][colunas];
 
-        Posicao posicaoAux = new Posicao();
-
-        //Mover para cima
+        this.moverParaCima(matrizPossiveisMovimentos);
         //Mover para baixo
         //Mover para esquerda
         //Mover para direita
         
         return matrizPossiveisMovimentos;
+    }
+
+    private void moverParaCima(boolean[][] matrizPossiveisMovimentos) {
+        Posicao posicaoAux = new Posicao();
+
+        //Mover para cima
+        posicaoAux.setLinha(this.posicao.getLinha() - 1);
+        posicaoAux.setColuna(this.posicao.getColuna());
+        while (true) {
+            boolean existePosicao = this.getTabuleiro().posicaoExiste(posicaoAux);
+            boolean isEspacoLivre = existePosicao && !this.getTabuleiro().existeUmaPecaNaPosicao(posicaoAux);
+            boolean temPecaDoOponente = existePosicao && !isEspacoLivre && this.getTabuleiro().existeUmaPecaNaPosicao(posicaoAux);
+            if (isEspacoLivre) {
+                matrizPossiveisMovimentos[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+                posicaoAux.setLinha(posicaoAux.getLinha() - 1);
+            } else if (temPecaDoOponente) {
+                matrizPossiveisMovimentos[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;                
+            } else {
+                break;
+            }
+        }
     }
 
 }
