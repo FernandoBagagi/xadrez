22b2df0b05e74833e1e4d4cfb3d69c6aa98a7901
Fernando <fernandosmartphone96@gmail.com>
2023-09-23 20:23:27 -0300

    formata App

--- src/aplicacao/App.java
+++ src/aplicacao/App.java
@@ -15,37 +15,36 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
         PartidaXadrez partidaXadrez = new PartidaXadrez();
-        List<PecaXadrez> pecasCapturadas = new ArrayList<>(); //Já tem na partidaXadrez
+        List<PecaXadrez> pecasCapturadas = new ArrayList<>(); // Já tem na partidaXadrez
 
         int numeroJogadas = 50;
 
         while (numeroJogadas-- != 0) {
             try {
                 UserInterface.limparTela();
-    
+
                 UserInterface.imprimirPartidaXadrez(partidaXadrez, pecasCapturadas);
-                
+
                 System.out.print("\nOrigem: ");
                 PosicaoXadrez origem = UserInterface.lerPosicaoXadrez(scanner);
-    
+
                 boolean[][] possiveisMovimentacoes = partidaXadrez.possiveisMovimentacoes(origem);
                 UserInterface.limparTela();
                 UserInterface.imprimirTabuleiro(partidaXadrez.getPecasXadrez(), possiveisMovimentacoes);
-                
 
                 System.out.print("\nDestino: ");
                 PosicaoXadrez destino = UserInterface.lerPosicaoXadrez(scanner);
                 System.out.println();
-                
-                PecaXadrez pecaCapturada = partidaXadrez.moverPecaXadrez(origem, destino);  
-                
-                if(pecaCapturada != null) {
+
+                PecaXadrez pecaCapturada = partidaXadrez.moverPecaXadrez(origem, destino);
+
+                if (pecaCapturada != null) {
                     pecasCapturadas.add(pecaCapturada);
                 }
 
             } catch (XadrezExcecao | InputMismatchException e) {
                 System.err.println(e.getMessage() + " Pressione enter para continuar");
-                scanner.nextLine(); //Esperar usuário da enter
+                scanner.nextLine(); // Esperar usuário da enter
             }
         }
 
