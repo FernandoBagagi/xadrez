d127059777d52f515b9e57932d0ecd8ee8eda895
Fernando Bagagi <fernandosmartphone96@gmail.com>
2023-10-23 15:29:26 -0300

    implementa movimentos do bispo

--- src/aplicacao/chess/pecas/Bispo.java
+++ src/aplicacao/chess/pecas/Bispo.java
@@ -1,5 +1,6 @@
 package aplicacao.chess.pecas;
 
+import aplicacao.boardgame.Posicao;
 import aplicacao.boardgame.Tabuleiro;
 import aplicacao.chess.Cor;
 import aplicacao.chess.PecaXadrez;
@@ -20,7 +21,78 @@ public class Bispo extends PecaXadrez {
         final int linhas = this.getTabuleiro().getLinhas();
         final int colunas = this.getTabuleiro().getColunas();
         boolean[][] matrizPossiveisMovimentos = new boolean[linhas][colunas];
+        this.moverParaNordeste(matrizPossiveisMovimentos);
+        this.moverParaSudeste(matrizPossiveisMovimentos);
+        this.moverParaSudoeste(matrizPossiveisMovimentos);
+        this.moverParaNoroeste(matrizPossiveisMovimentos);
         return matrizPossiveisMovimentos;
     }
 
+    private void moverParaNordeste(boolean[][] matrizPossiveisMovimentos) {
+        Posicao posicaoAux = new Posicao(this.posicao.getLinha() - 1, this.posicao.getColuna() + 1);
+        while (true) {
+            boolean existePosicao = this.getTabuleiro().posicaoExisteTratado(posicaoAux);
+            boolean isEspacoLivre = existePosicao && !this.getTabuleiro().existeUmaPecaNaPosicao(posicaoAux);
+            boolean temPecaDoOponente = existePosicao && !isEspacoLivre && this.existePecaDoOponenteNa(posicaoAux);
+            if (isEspacoLivre || temPecaDoOponente) {
+                matrizPossiveisMovimentos[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+            }
+            if(!isEspacoLivre || temPecaDoOponente) {
+                break;
+            }
+            posicaoAux.setLinha(posicaoAux.getLinha() - 1);
+            posicaoAux.setColuna(posicaoAux.getColuna() + 1);
+        }
+    }
+
+    private void moverParaSudeste(boolean[][] matrizPossiveisMovimentos) {
+        Posicao posicaoAux = new Posicao(this.posicao.getLinha() + 1, this.posicao.getColuna() + 1);
+        while (true) {
+            boolean existePosicao = this.getTabuleiro().posicaoExisteTratado(posicaoAux);
+            boolean isEspacoLivre = existePosicao && !this.getTabuleiro().existeUmaPecaNaPosicao(posicaoAux);
+            boolean temPecaDoOponente = existePosicao && !isEspacoLivre && this.existePecaDoOponenteNa(posicaoAux);
+            if (isEspacoLivre || temPecaDoOponente) {
+                matrizPossiveisMovimentos[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+            }
+            if(!isEspacoLivre || temPecaDoOponente) {
+                break;
+            }
+            posicaoAux.setLinha(posicaoAux.getLinha() + 1);
+            posicaoAux.setColuna(posicaoAux.getColuna() + 1);
+        }
+    }
+
+    private void moverParaSudoeste(boolean[][] matrizPossiveisMovimentos) {
+        Posicao posicaoAux = new Posicao(this.posicao.getLinha() + 1, this.posicao.getColuna() - 1);
+        while (true) {
+            boolean existePosicao = this.getTabuleiro().posicaoExisteTratado(posicaoAux);
+            boolean isEspacoLivre = existePosicao && !this.getTabuleiro().existeUmaPecaNaPosicao(posicaoAux);
+            boolean temPecaDoOponente = existePosicao && !isEspacoLivre && this.existePecaDoOponenteNa(posicaoAux);
+            if (isEspacoLivre || temPecaDoOponente) {
+                matrizPossiveisMovimentos[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+            }
+            if(!isEspacoLivre || temPecaDoOponente) {
+                break;
+            }
+            posicaoAux.setLinha(posicaoAux.getLinha() + 1);
+            posicaoAux.setColuna(posicaoAux.getColuna() - 1);
+        }
+    }
+
+    private void moverParaNoroeste(boolean[][] matrizPossiveisMovimentos) {
+        Posicao posicaoAux = new Posicao(this.posicao.getLinha() - 1, this.posicao.getColuna() - 1);
+        while (true) {
+            boolean existePosicao = this.getTabuleiro().posicaoExisteTratado(posicaoAux);
+            boolean isEspacoLivre = existePosicao && !this.getTabuleiro().existeUmaPecaNaPosicao(posicaoAux);
+            boolean temPecaDoOponente = existePosicao && !isEspacoLivre && this.existePecaDoOponenteNa(posicaoAux);
+            if (isEspacoLivre || temPecaDoOponente) {
+                matrizPossiveisMovimentos[posicaoAux.getLinha()][posicaoAux.getColuna()] = true;
+            }
+            if(!isEspacoLivre || temPecaDoOponente) {
+                break;
+            }
+            posicaoAux.setLinha(posicaoAux.getLinha() - 1);
+            posicaoAux.setColuna(posicaoAux.getColuna() - 1);
+        }
+    }
 }
